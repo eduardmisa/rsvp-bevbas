249b5aa70e04cdddd06981a7ac2f3766553db6bc
eduardmisa <eduardmisa@gmail.com>
2023-08-20 11:04:22 +0800

    fix: auto scrolling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 "use client"
 import useSound from 'use-sound';
-import ReactFullpage from '@fullpage/react-fullpage';
+import ReactFullpage, { fullpageApi } from '@fullpage/react-fullpage';
 import {First} from "@/app/slides/First";
 import {Second} from "@/app/slides/Second";
 import {Third} from "@/app/slides/Third";
 import {Fourth} from "@/app/slides/Fourth";
 import {Fifth} from "@/app/slides/Fifth";
+import {useEffect, useState} from "react";
 
 export default function Home() {
   const [ play ] = useSound(
@@ -17,10 +18,21 @@ export default function Home() {
 
   void play();
 
+  const [isFullpageApiMounted, setIsFullpageApiMounted] = useState(false)
+  const [fullpageApi, setFullpageApi] = useState<{ state: any; fullpageApi: fullpageApi} | undefined>();
+  useEffect(() => {
+    if (fullpageApi && !isFullpageApiMounted) {
+      setInterval(() => {
+        fullpageApi?.fullpageApi.moveSectionDown();
+      }, 3000);
+      setIsFullpageApiMounted(true)
+    }
+  }, [fullpageApi])
+
   return (
     <main className="flex flex-col min-h-screen min-w-screen max-h-screen max-w-screen">
       <ReactFullpage
-          // key={"MOBILE"}
+          key={"MOBILE"}
           credits={{
             enabled: true,
             position: "right"
@@ -31,25 +43,28 @@ export default function Home() {
           scrollingSpeed={600}
           verticalCentered
           fitToSection
-          render={() => (
-              <ReactFullpage.Wrapper>
-                <div className="section">
-                  <First />
-                </div>
-                <div className="section">
-                  <Second />
-                </div>
-                <div className="section">
-                  <Third />
-                </div>
-                <div className="section">
-                  <Fourth />
-                </div>
-                <div className="section">
-                  <Fifth />
-                </div>
-              </ReactFullpage.Wrapper>
-          )}
+          render={(obj) =>  {
+            setFullpageApi(obj);
+            return (
+                <ReactFullpage.Wrapper>
+                  <div className="section">
+                    <First />
+                  </div>
+                  <div className="section">
+                    <Second />
+                  </div>
+                  <div className="section">
+                    <Third />
+                  </div>
+                  <div className="section">
+                    <Fourth />
+                  </div>
+                  <div className="section">
+                    <Fifth />
+                  </div>
+                </ReactFullpage.Wrapper>
+            )
+          }}
       />
     </main>
   )
